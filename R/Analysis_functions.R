41cb85270d214e367f21a7febbc3dc9ae970c5b8
MichaelGurkov <michael.gurkov@mail.huji.ac.il>
2019-03-06 18:19:08 +0200

    Initial commit of analysis functions

---
+++ R/Analysis_functions.R
@@ -0,0 +1,53 @@
+#' This function performs regression alternating between "pop" and "gdp"
+#' specs of explanatory vars
+#'
+#' @import plm
+#'
+#' @import dplyr
+#'
+#' @export
+
+
+get.reg.list = function(reg_df, dep_var_name){
+
+  x_vars_pop = paste("bank_pop","trade_pop","LGDP","LPop",
+                     "Pop_diff","GDP_per_Capita_real_diff",
+                     "FX_diff",sep = "+")
+
+  x_vars_pop = names(reg_df)[names(reg_df) %in% x_vars_pop]
+
+  x_vars_gdp = paste("bank_gdp","trade_gdp","LGDP","LPop",
+                     "Pop_diff","GDP_per_Capita_real_diff",
+                     "FX_diff",sep = "+")
+
+  x_vars_gdp = names(reg_df)[names(reg_df) %in% gdp]
+
+  pop_reg = plm(formula(paste(dep_var_name, x_vars_pop, sep = "~")),
+                data = reg_df, model = "within", effect = "twoways")
+
+  gdp_reg = plm(formula(paste(dep_var_name, x_vars_gdp, sep = "~")),
+                data = reg_df, model = "within", effect = "twoways")
+
+  return(list(pop_reg = pop_reg, gdp_reg = gdp_reg))
+}
+
+
+#' This function performs regressions on every dep var in the data frame
+#'
+#' @import plm
+#'
+#' @import dplyr
+#'
+#' @export
+
+get.sens.reg = function(reg_df){
+
+  dep_vars = names(reg_df)[!grepl("bank|trade|LGDP|LPop|DGDP|CountryPair|Date",
+                           names(reg_df))]
+
+  sens_reg = lapply(dep_vars, get.reg.list, reg_df = reg_df)
+
+  names(sens_reg) = dep_vars
+
+  return(sens_reg)
+}
