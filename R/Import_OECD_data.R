f4262f98dbfd8a1d8cc6a2776615956a9b6e6577
MichaelGurkov <michael.gurkov@mail.huji.ac.il>
2019-10-22 19:49:50 +0300

    update of import_oecd data

--- R/Import_OECD_data.R
+++ R/Import_OECD_data.R
@@ -42,15 +42,61 @@
 import.oecd.houseprice = function(
   filepath = paste0("C:\\Users\\Misha\\Documents\\Data",
                     "\\OECD\\House_prices.csv"),
-  my_subject = "REAL", my_frequency = "Q"){
+  my_frequency = "Q"){
 
   df = read.csv(file = filepath, stringsAsFactors = FALSE)
 
   df = df %>%
-  filter(SUBJECT == my_subject) %>%
+    filter(FREQUENCY == my_frequency) %>%
+    select(ï..LOCATION,TIME, Value) %>%
+    rename(Country = ï..LOCATION, Date = TIME, HousePrice = Value) %>%
+    mutate(Date = as.yearqtr(Date, format = "%Y-Q%q"))
+
+  return(df)
+
+}
+
+
+#' Import OECD share price dataframe
+#'
+#'
+#' This function imports share price data from OECD format.
+#'
+#'
+#' @details
+#' Share price indices are calculated from the prices of common shares of
+#' companies traded on national or foreign stock exchanges. They are usually
+#' determined by the stock exchange, using the closing daily values for the
+#' monthly data, and normally expressed as simple arithmetic averages of the
+#' daily data. A share price index measures how the value of the stocks in the
+#' index is changing, a share return index tells the investor what their
+#' “return” is, meaning how much money they would make as a result of investing
+#' in that basket of shares. A price index measures changes in the market
+#' capitalisation of the basket of shares in the index whereas a return index
+#' adds on to the price index the value of dividend payments, assuming they
+#' are re-invested in the same stocks. Occasionally agencies such as central
+#' banks will compile share indices.
+#'
+#' @import dplyr
+#'
+#' @import zoo
+#'
+#'
+#' @param my_frequency filters the required frequency
+#' (annual - A , quarter - Q or monthly - M). The default is Q
+
+
+import.oecd.shareprice = function(
+  filepath = paste0("C:\\Users\\Misha\\Documents\\Data",
+                    "\\OECD\\Share_prices.csv"),
+  my_frequency = "Q"){
+
+  df = read.csv(file = filepath, stringsAsFactors = FALSE)
+
+  df = df %>%
   filter(FREQUENCY == my_frequency) %>%
   select(ï..LOCATION,TIME, Value) %>%
-  rename(Country = ï..LOCATION, Date = TIME, HousePrice = Value) %>%
+  rename(Country = ï..LOCATION, Date = TIME, SharePrice = Value) %>%
   mutate(Date = as.yearqtr(Date, format = "%Y-Q%q"))
 
   return(df)
@@ -85,7 +131,7 @@ import.oecd.houseprice = function(
 #' @param my_measure filters the required measurement type
 #' (percentage change from the previous quarter  - PC_CHGPP,
 #'  percentage change from the same quarter of the previous year - PC_CHGPY
-#'  and volume index - IDX.) The default is PC_CHGPY.
+#'  and volume index - IDX.) The default is PC_CHGPP.
 #'
 #' @param my_frequency filters the required frequency
 #' (annual - A or quarter - Q). The default is Q
@@ -93,7 +139,7 @@ import.oecd.houseprice = function(
 import.oecd.gdp.quarter = function(
   filepath = paste0("C:\\Users\\Misha\\Documents\\Data",
                     "\\OECD\\GDP_quarterly.csv"),
-  my_subject = "TOT", my_measure = "PC_CHGPY", my_frequency = "Q"){
+  my_subject = "TOT", my_measure = "PC_CHGPP", my_frequency = "Q"){
 
   df = read.csv(file = filepath, stringsAsFactors = FALSE)
 
@@ -101,6 +147,47 @@ import.oecd.gdp.quarter = function(
     filter(SUBJECT == my_subject) %>%
     filter(MEASURE == my_measure) %>%
     filter(FREQUENCY == my_frequency) %>%
+    select(ï..LOCATION,TIME, Value) %>%
+    rename(Country = ï..LOCATION, Date = TIME, GDP = Value) %>%
+    mutate(Date = as.yearqtr(Date, format = "%Y-Q%q"))
+
+  return(df)
+
+}
+
+
+
+#' Import OECD GDP dataframe
+#'
+#'
+#' This function imports (nominal) GDP data from OECD format.
+#'
+#'
+#' @details
+#' This indicator is based on nominal GDP (also called GDP at current prices
+#' or GDP in value) and is available in different measures: US dollars and US
+#' dollars per capita (current PPPs). All OECD countries compile their data
+#' according to the 2008 System of National Accounts (SNA). This indicator is
+#' less suited for comparisons over time, as developments are not only caused
+#' by real growth, but also by changes in prices and PPPs.
+#'
+#' @import dplyr
+#'
+#' @import zoo
+#'
+#' @param my_measure filters the required measure
+#' (US dollars - MLN_USD , US dollars per capita (current PPPs) - USD_CAP).
+#' The default is MLN_USD
+
+
+import.oecd.gdp = function(
+  filepath ="C:\\Users\\Misha\\Documents\\Data\\OECD\\GDP.csv",
+  my_measure = "MLN_USD"){
+
+  df = read.csv(file = filepath, stringsAsFactors = FALSE)
+
+  df = df %>%
+    filter(MEASURE == my_measure) %>%
     select(ï..LOCATION,TIME, Value) %>%
     rename(Country = ï..LOCATION, Date = TIME, GDP = Value) %>%
     mutate(Date = as.yearqtr(Date, format = "%Y-Q%q"))
