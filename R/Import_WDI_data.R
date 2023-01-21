c21508382a2b016cb61d4cf997f41842df71fc29
Michael Gurkov <michael.gurkov@mail.huji.ac.il>
2023-01-21 22:51:03 +0200

    update lbs data preprocessing

--- R/Import_WDI_data.R
+++ R/Import_WDI_data.R
@@ -11,8 +11,8 @@
 #' rate effectively applied to actual foreign exchange transactions, an
 #' alternative conversion factor is used. World Bank ID is NY.GDP.MKTP.CD
 #'
-#' Constant data are in constant 2010 U.S. dollars. Dollar figures for GDP are
-#' converted from domestic currencies using 2010 official exchange rates.
+#' Constant data are in constant 2015 U.S. dollars. Dollar figures for GDP are
+#' converted from domestic currencies using 2015 official exchange rates.
 #' For a few countries where the official exchange rate does not reflect the
 #' rate effectively applied to actual foreign exchange transactions, an
 #' alternative conversion factor is used. World Bank ID is NY.GDP.MKTP.KD
@@ -26,23 +26,28 @@
 #' @param countries_codes vector of codes of required countries
 
 
-import.wdi.gdp = function(filepath,countries_codes = NULL,
-                          message_gdp_type = TRUE){
+import_wdi_gdp = function(file_path){
 
-  df = read.csv(file = filepath, stringsAsFactors = FALSE, skip = 4)
+  bis_oecd_codes_df = raw_data %>%
+    pluck("bis_codes") %>%
+    filter(oecd_member == 1) %>%
+    select(country) %>%
+    mutate(country = str_replace(country,"Turkey","Turkiye"))
 
-  if(message_gdp_type){
+  raw_df = read_csv(file = file_path, show_col_types = FALSE, skip = 4)
 
-    if(length(df$Indicator.Name[1]) ==1){message(df$Indicator.Name[1])}
-  }
+
+  df = raw_df %>%
+    select(country = `Country Name`, matches("[0-9]{4}")) %>%
+    mutate(country = str_replace_all(country, " ","_")) %>%
+    mutate(country = str_replace(country, "Korea,_Rep\\.","Korea")) %>%
+    mutate(country = str_replace(country, "Slovak_Republic","Slovakia")) %>%
+    inner_join(bis_oecd_codes_df, by = "country") %>%
+    pivot_longer(-country,names_to = "year",values_to = "gdp_usd")
 
   df = df %>%
-    select(Country.Name, Country.Code, starts_with("X")) %>%
-    rename(Country = Country.Name, Code = Country.Code) %>%
-    select_if(.predicate = list(~!all(is.na(.)))) %>%
-    rename_all(.funs = list(~gsub("X","",.))) %>%
-    {if(!is.null(countries_codes)) filter(.,Code %in% countries_codes) else .} %>%
-    gather(key = Date,value = GDP,-Country,-Code)
+    mutate(gdp_usd = gdp_usd * 10 ^ (-6))
+
 
   return(df)
 
